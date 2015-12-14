da8c84267b1228a59226e8436609d403d88c3961
Nikos Roussos <nikos@roussos.cc>
2015-12-14 14:05:12 +0200

    First iteration on gallery thumbs

--- athensreport/gallery/urls.py
+++ athensreport/gallery/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns(
     'athensreport.gallery.views',
     url(r'^theproject/$', 'theproject', name='theproject'),
+    url(r'^graffiti/$', 'graffiti', name='graffiti'),
 )
--- athensreport/gallery/views.py
+++ athensreport/gallery/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from athensreport.gallery.models import Item
 
 def theproject(request):
     """View to render the project page."""
     return render(request, 'gallery/theproject.html')
+
+
+def graffiti(request):
+    """View to render the graffiti page."""
+    items = Item.objects.filter(category='Graffiti')
+    return render(request, 'gallery/theproject.html', {'items': items})
