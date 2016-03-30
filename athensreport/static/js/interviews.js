7d7434af37ca5196ea2641b5d3c5f7fa7088af96
Nikos Roussos <nikos@roussos.cc>
2016-03-30 13:47:54 +0300

    Add interviews carousel and more elements

--- athensreport/static/js/interviews.js
+++ athensreport/static/js/interviews.js
@@ -37,21 +37,83 @@ $(document).ready(function() {
                 var info = $('#details-info');
                 var script = $('#details-script');
                 var source_html = `
-                    <img src="/media/${item.fields.photo1}" alt="${item.fields.name}" class="img-responsive">
+                    <div id="carousel-interviews" class="carousel slide" data-ride="carousel">
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                        <div class="item active"><img src="/media/${item.fields.photo1}" alt="${item.fields.name}"></div>
+                `;
+                if (item.fields.photo2) {
+                    source_html += `<div class="item"><img src="/media/${item.fields.photo2}" alt="${item.fields.name}"></div>`;
+                }
+                if (item.fields.photo3) {
+                    source_html += `<div class="item"><img src="/media/${item.fields.photo3}" alt="${item.fields.name}"></div>`;
+                }
+                if (item.fields.photo4) {
+                    source_html += `<div class="item"><img src="/media/${item.fields.photo4}" alt="${item.fields.name}"></div>`;
+                }
+                if (item.fields.photo5) {
+                    source_html += `<div class="item"><img src="/media/${item.fields.photo5}" alt="${item.fields.name}"></div>`;
+                }
+                if (item.fields.photo6) {
+                    source_html += `<div class="item"><img src="/media/${item.fields.photo6}" alt="${item.fields.name}"></div>`;
+                }
+                if (item.fields.photo7) {
+                    source_html += `<div class="item"><img src="/media/${item.fields.photo7}" alt="${item.fields.name}"></div>`;
+                }
+                if (item.fields.photo8) {
+                    source_html += `<div class="item"><img src="/media/${item.fields.photo8}" alt="${item.fields.name}"></div>`;
+                }
+                if (item.fields.photo9) {
+                    source_html += `<div class="item"><img src="/media/${item.fields.photo9}" alt="${item.fields.name}"></div>`;
+                }
+                if (item.fields.photo10) {
+                    source_html += `<div class="item"><img src="/media/${item.fields.photo10}" alt="${item.fields.name}"></div>`;
+                }
+
+                source_html += `
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carousel-interviews" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-interviews" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
                 `;
                 var year = item.fields.created.substr(0, 4);
                 var info_html = `
                     <div class="gallery-details-text">
                     <div class="gallery-title">${item.fields.name}</div>
-                    <div class="gallery-year">
+                    <div class="gallery-year yellow-dark">
                       ${year}
                     </div>
+                    `;
+                if (item.fields.location) {
+                    info_html += `
                     <div>
-                      ${item.fields.location}
+                      <label>Location:</label> ${item.fields.location}
                     </div>
+                    `;
+                }
+                if (item.fields.age) {
+                    info_html += `
                     <div>
-                      ${item.fields.age}
+                      <label>Age:</label> ${item.fields.age}
                     </div>
+                    `;
+                }
+                if (item.fields.sex) {
+                    info_html += `
+                    <div>
+                      <label>Sex:</label> ${item.fields.sex}
+                    </div>
+                    `;
+                }
+                info_html += `
                     <div><audio src="/media/${item.fields.mp3_en}" controls></div>
                     <div><audio src="/media/${item.fields.mp3_gr}" controls></div>
                     </div>
@@ -76,15 +138,12 @@ $(document).ready(function() {
                     scrollTop: $(target).offset().top - 130
                 }, 1000);
             });
-            $(this).trigger('detailsloaded');
         });
     });
 
-    $('.details').on('detailsloaded', function() {
-        $('#details-script-reveal').on('click', function(event) {
-            event.preventDefault();
-            $('#details-script').show();
-        });
+    $(document).on('click', '#details-script-reveal', function(event) {
+        event.preventDefault();
+        $('.script').toggle();
     });
 
     // Render gallery
