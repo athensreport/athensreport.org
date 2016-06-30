f1387bfdb55e0727c575416f2d787bc54b654c4a
Nikos Roussos <nikos@roussos.cc>
2016-06-30 14:37:26 +0300

    Add analytics

--- athensreport/base/context_processors.py
+++ athensreport/base/context_processors.py
@@ -1,8 +1,17 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
+from django.template.loader import render_to_string
 
 
 def current_site_url(request):
     current_site = Site.objects.get_current()
     url = '{0}{1}'.format(current_site.domain, request.path)
     return {'current_url': url}
+
+def analytics(request):
+    """Returns analytics code."""
+    if settings.ENVIRONMENT == 'production':
+        return {'analytics_code': render_to_string('includes/analytics.html',
+                {'google_analytics_key': settings.GOOGLE_ANALYTICS_KEY})}
+    else:
+        return {'analytics_code': ''}
--- athensreport/settings/base.py
+++ athensreport/settings/base.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
+                'athensreport.base.context_processors.analytics',
                 'athensreport.base.context_processors.current_site_url',
             ],
             'loaders': [
