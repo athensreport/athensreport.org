822d9cf80bef86b30b36dff57c00e125d6295d26
Nikos Roussos <nikos@roussos.cc>
2018-09-12 23:48:26 +0300

    Upgrade requirements

--- athensreport/base/urls.py
+++ athensreport/base/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 
 from athensreport.base import views
 
--- athensreport/gallery/urls.py
+++ athensreport/gallery/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 
 from athensreport.gallery import views
 
